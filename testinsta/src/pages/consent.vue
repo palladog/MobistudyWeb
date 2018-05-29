d820fe76f99157985793e58d2cc034ae994c2a44
Arvin <arvingo@yahoo.co.uk>
2018-05-29 11:21:56 +0100

    Added fields to consent
    Added several fields to consent

--- testinsta/src/pages/consent.vue
+++ testinsta/src/pages/consent.vue
@@ -7,10 +7,29 @@
       label="Consent"
       />
     </div>
+    <div class="title-descr-container">
+      <form action="">
+        <div class="form-rowdesc">
+          <label for="invimessage">INVITATION MESSAGE:</label>
+          <input type="text" id="inviMessage">
+        </div>
+        <div class="form-rowdesc">
+          <label for="layDescr">LAY DESCRIPTION:</label>
+          <input type="text" id="layDescription">
+        </div>
+      </form>
+    </div>
+    <div class="privacy-policy-container">
+      <form action="">
+        <div class="form-rowdesc">
+          <label for="privapolicy">PRIVACY POLICY:</label>
+          <input type="text" id="privacyPolicy">
+        </div>
+      </form>
+    </div>
     <div class="next-container">
       <q-btn class="nextbutton"
-      label="Next Page"
-      icon-right="arrow_forward"
+      label="REVIEW BEFORE SUBMISSION"
       color="orange"
       @click="gotopagecriteria"
       >
@@ -20,6 +39,49 @@
 </template>
 
 <style>
+/* Invitation description */
+.title-descr-container {
+  max-width: 750px;
+  margin: 20px auto 0 auto;
+  padding: 30px;
+  background-color: #FFF444;
+}
+
+.form-row {
+  padding: 10px 0;
+  height: 50px;
+  display: flex;
+}
+
+.form-row label {
+  padding-right: 190px;
+}
+
+.form-row input {
+  flex: 1;
+}
+
+.form-rowdesc {
+  padding: 10px 0;
+  height: 250px;
+  display: flex;
+}
+
+.form-rowdesc label {
+  padding-right: 100px;
+}
+
+.form-rowdesc input {
+  flex: 1;
+}
+
+.privacy-policy-container {
+  max-width: 750px;
+  margin: 20px auto 0 auto;
+  padding: 30px;
+  background-color: rgb(255, 211, 68);
+}
+
 /*Next button*/
 .next-container{
   max-width: 750px;
