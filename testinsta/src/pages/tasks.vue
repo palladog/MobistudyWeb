aed77aa3775b75f6f75bb5ea0e52366a68971d25
Arvin <arvingo@yahoo.co.uk>
2018-06-05 17:50:33 +0100

    Added Dropdown to tasks
    Added Dropdown to tasks

--- testinsta/src/pages/tasks.vue
+++ testinsta/src/pages/tasks.vue
@@ -10,10 +10,32 @@
     </div>
     <div class="data-query-container">
       <form action="">
-        <div class="form-rowdesc">
-          <label for="dataquery">DATA QUERY:</label>
-          <input type="text" id="dataQuery">
-        </div>
+        <ul class="flex-outer" id="data-query-form">
+          <li>
+            <label for="data-query-input">Data Query:</label>
+             <q-btn-dropdown color="primary" label="Types of Data">
+              <q-list link>
+                <q-item v-close-overlay @click.native="showNotification">
+                  <q-item-side icon="folder" inverted color="primary" />
+                  <q-item-main>
+                    <q-item-tile label>Steps</q-item-tile>
+                    <q-item-tile sublabel>February 22, 2016</q-item-tile>
+                  </q-item-main>
+                  <q-item-side right icon="info" color="amber" />
+                </q-item>
+                <q-item-separator inset />
+                <q-item v-close-overlay @click.native="showNotification">
+                  <q-item-side icon="assignment" inverted color="secondary" />
+                  <q-item-main>
+                    <q-item-tile label>Vacation</q-item-tile>
+                    <q-item-tile sublabel>February 22, 2016</q-item-tile>
+                  </q-item-main>
+                  <q-item-side right icon="info" color="amber" />
+                </q-item>
+              </q-list>
+            </q-btn-dropdown>
+          </li>
+        </ul>
       </form>
     </div>
     <div class="questionnaire-container">
@@ -44,6 +66,27 @@
 </template>
 
 <style>
+/* Flex*/
+.flex-outer li {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+
+.flex-outer > li > label,
+.flex-outer li p {
+  flex: 1 0 120px;
+  max-width: 220px;
+}
+
+.flex-outer > li > label + * {
+  flex: 1 0 220px;
+}
+
+/* Spacing between*/
+.flex-outer > li:not(:last-child) {
+  margin-bottom: 20px;
+}
 /* Data Query*/
 .data-query-container {
   max-width: 750px;
@@ -120,6 +163,9 @@ export default {
     },
     gotopageConsent () {
       this.$router.push('pageConsent')
+    },
+    showNotification () {
+      this.$q.notify('Option selected & closed dropdown')
     }
   }
 }
