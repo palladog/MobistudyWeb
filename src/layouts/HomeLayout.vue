897eb4ff7e377d352d7a637fc8028cbe15f3e233
Dario Salvi <dario.salvi.work@gmail.com>
2018-12-14 16:44:44 +0000

    added website version to the home layout

--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -4,7 +4,7 @@
       <q-toolbar color="primary" :glossy="$q.theme === 'mat'" :inverted="$q.theme === 'ios'" >
         <q-toolbar-title>
           MobiStudy
-          <span slot="subtitle">Running on Quasar v{{ $q.version }}</span>
+          <span slot="subtitle">Version {{ version }}</span>
         </q-toolbar-title>
         <q-btn flat round dense icon="exit_to_app" @click="logout()" aria-label="Logout" />
       </q-toolbar>
@@ -17,11 +17,17 @@
 </template>
 
 <script>
+import { version } from '../../package.json'
 import API from '../data/API.js'
 import userinfo from '../data/userinfo.js'
 
 export default {
   name: 'HomeLayout',
+  data () {
+    return {
+      version: version
+    }
+  },
   methods: {
     logout () {
       userinfo.logout()
