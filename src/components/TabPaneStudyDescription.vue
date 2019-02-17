7e1804eaf35387985e02aa10b29eddaf037f746f
Unknown <arvingob@gmail.com>
2019-02-17 14:06:01 +0000

    Added Tab Pane Study Description

---
+++ src/components/TabPaneStudyDescription.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-tab-pane name="tab-study-description">
+    <q-card>
+      <q-card-title> Study Description1
+      </q-card-title>
+      <q-card-main>
+        <div class="row justify-around">
+           {{ studyDesign }}
+        </div>
+      </q-card-main>
+    </q-card>
+
+  </q-tab-pane>
+</template>
+
+<script>
+
+export default {
+  name: 'TabPaneStudyDescription',
+  props: ['studyDesign'],
+  data () {
+    return {
+      participants: {
+        joined: 0,
+        active: 0,
+        completed: 0,
+        withdrawn: 0
+      }
+    }
+  },
+
+  methods: {
+    async downloadAnswers () {
+      // TODO
+    }
+  }
+}
+</script>
