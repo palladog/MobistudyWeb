f364d8e974f535708f94027b43fe2765f4bd6732
Dario Salvi <dariosalvi78@gmail.com>
2020-12-11 13:34:20 +0100

    missing reasonForDataAccess when adding institution

--- src/components/StudyDesignGeneralities.vue
+++ src/components/StudyDesignGeneralities.vue
@@ -16,9 +16,24 @@
             </div>
           </div>
           <div class="col q-pl-sm">
-            <q-field :error="v.languages.$error" error-message="At least one language must be specified">
-            <q-checkbox v-model.trim="v.languages.$model" @blur="v.languages.$touch" @input="update()" val="en" label="English" />
-            <q-checkbox v-model.trim="v.languages.$model" @blur="v.languages.$touch" @input="update()" val="sv" label="Swedish" />
+            <q-field
+              :error="v.languages.$error"
+              error-message="At least one language must be specified"
+            >
+              <q-checkbox
+                v-model.trim="v.languages.$model"
+                @blur="v.languages.$touch"
+                @input="update()"
+                val="en"
+                label="English"
+              />
+              <q-checkbox
+                v-model.trim="v.languages.$model"
+                @blur="v.languages.$touch"
+                @input="update()"
+                val="sv"
+                label="Swedish"
+              />
             </q-field>
           </div>
         </div>
@@ -32,7 +47,13 @@
             </div>
           </div>
           <div class="col q-pl-sm">
-            <q-input-multilang v-model.trim="v.title.$model" @blur="v.title.$touch" @input="update()" :languages="value.languages" required/>
+            <q-input-multilang
+              v-model.trim="v.title.$model"
+              @blur="v.title.$touch"
+              @input="update()"
+              :languages="value.languages"
+              required
+            />
           </div>
         </div>
         <div class="row">
@@ -45,7 +66,13 @@
             </div>
           </div>
           <div class="col q-pl-sm">
-            <q-input-multilang v-model.trim="v.shortDescription.$model" @blur="v.shortDescription.$touch" @input="update()" :languages="value.languages" required/>
+            <q-input-multilang
+              v-model.trim="v.shortDescription.$model"
+              @blur="v.shortDescription.$touch"
+              @input="update()"
+              :languages="value.languages"
+              required
+            />
           </div>
         </div>
         <div class="row">
@@ -58,19 +85,29 @@
             </div>
           </div>
           <div class="col q-pl-sm">
-            <q-input-multilang type="textarea" v-model.trim="v.longDescription.$model" @blur="v.longDescription.$touch" @input="update()" :languages="value.languages" required/>
+            <q-input-multilang
+              type="textarea"
+              v-model.trim="v.longDescription.$model"
+              @blur="v.longDescription.$touch"
+              @input="update()"
+              :languages="value.languages"
+              required
+            />
           </div>
         </div>
       </q-card-section>
     </q-card>
-<!-- Gen Info Tab: Principal Investigator Card -->
+    <!-- Gen Info Tab: Principal Investigator Card -->
     <q-card class="form-card">
       <q-card-section>
         <div class="text-h5"> Principal Investigator(s) </div>
         <div class="text-subtitle1"> Principal investigators of this study. </div>
       </q-card-section>
       <q-card-section>
-        <div v-for="(pi, index) in v.principalInvestigators.$each.$iter" :key="index">
+        <div
+          v-for="(pi, index) in v.principalInvestigators.$each.$iter"
+          :key="index"
+        >
           <div class="row">
             <div class="col-4 q-pt-lg">
               <div class="text-bold">
@@ -81,10 +118,15 @@
               </div>
             </div>
             <div class="col q-pl-sm">
-              <q-input type="text"
-              hint="Same text for all languages"
-              v-model.trim="pi.name.$model" @blur="pi.name.$touch" @input="update()"
-              :error="pi.name.$error" error-message="Field is required."/>
+              <q-input
+                type="text"
+                hint="Same text for all languages"
+                v-model.trim="pi.name.$model"
+                @blur="pi.name.$touch"
+                @input="update()"
+                :error="pi.name.$error"
+                error-message="Field is required."
+              />
             </div>
           </div>
           <div class="row">
@@ -97,10 +139,16 @@
               </div>
             </div>
             <div class="col q-pl-sm">
-              <q-input type="textarea" autogrow
-              hint="Same text for all languages"
-              v-model.trim="pi.contact.$model" @blur="pi.contact.$touch" @input="update()"
-              :error="pi.contact.$error" error-message="Field is required."/>
+              <q-input
+                type="textarea"
+                autogrow
+                hint="Same text for all languages"
+                v-model.trim="pi.contact.$model"
+                @blur="pi.contact.$touch"
+                @input="update()"
+                :error="pi.contact.$error"
+                error-message="Field is required."
+              />
             </div>
           </div>
           <div class="row">
@@ -113,32 +161,57 @@
               </div>
             </div>
             <div class="col q-pl-sm">
-              <q-input type="text"
-              hint="Same text for all languages"
-              v-model.trim="pi.institution.$model" @blur="pi.institution.$touch" @input="update()"
-              :error="pi.institution.$error" error-message="Field is required."/>
+              <q-input
+                type="text"
+                hint="Same text for all languages"
+                v-model.trim="pi.institution.$model"
+                @blur="pi.institution.$touch"
+                @input="update()"
+                :error="pi.institution.$error"
+                error-message="Field is required."
+              />
             </div>
           </div>
           <div class="row">
             <div class="col">
-              <q-btn v-show="index !=0" label="Remove PI" color="negative" icon="remove" @click="removeRowInvestigator(index)"/>
+              <q-btn
+                v-show="index !=0"
+                label="Remove PI"
+                color="negative"
+                icon="remove"
+                @click="removeRowInvestigator(index)"
+              />
             </div>
             <div class="col">
-              <q-btn class="float-right" v-show="index == value.principalInvestigators.length-1" label="Add PI" color="primary" icon="add" @click="addRowInvestigator(index)"/>
+              <q-btn
+                class="float-right"
+                v-show="index == value.principalInvestigators.length-1"
+                label="Add PI"
+                color="primary"
+                icon="add"
+                @click="addRowInvestigator(index)"
+              />
             </div>
           </div>
-          <q-separator color="primary" spaced="xl" v-show="index != value.principalInvestigators.length-1"/>
+          <q-separator
+            color="primary"
+            spaced="xl"
+            v-show="index != value.principalInvestigators.length-1"
+          />
         </div>
       </q-card-section>
     </q-card>
-<!-- Gen Info Tab: Institutions Card -->
+    <!-- Gen Info Tab: Institutions Card -->
     <q-card class="form-card">
       <q-card-section>
         <div class="text-h5">Institution(s)</div>
         <div class="text-subtitle1"> Involved institutions and data access rules. </div>
       </q-card-section>
       <q-card-section>
-        <div v-for="(inst, index) in v.institutions.$each.$iter" :key="index">
+        <div
+          v-for="(inst, index) in v.institutions.$each.$iter"
+          :key="index"
+        >
           <div class="row">
             <div class="col-4 q-pt-lg">
               <div class="text-bold">
@@ -149,10 +222,15 @@
               </div>
             </div>
             <div class="col q-pl-sm">
-              <q-input type="text"
-              hint="Same text for all languages."
-              v-model.trim="inst.name.$model" @blur="inst.name.$touch" @input="update()"
-              :error="inst.name.$error" error-message="Field is required."/>
+              <q-input
+                type="text"
+                hint="Same text for all languages."
+                v-model.trim="inst.name.$model"
+                @blur="inst.name.$touch"
+                @input="update()"
+                :error="inst.name.$error"
+                error-message="Field is required."
+              />
             </div>
           </div>
           <div class="row">
@@ -165,10 +243,16 @@
               </div>
             </div>
             <div class="col q-pl-sm">
-              <q-input type="textarea" autogrow
-              hint="Same text for all languages."
-              v-model.trim="inst.contact.$model" @blur="inst.contact.$touch" @input="update()"
-              :error="inst.contact.$error" error-message="Field is required"/>
+              <q-input
+                type="textarea"
+                autogrow
+                hint="Same text for all languages."
+                v-model.trim="inst.contact.$model"
+                @blur="inst.contact.$touch"
+                @input="update()"
+                :error="inst.contact.$error"
+                error-message="Field is required"
+              />
             </div>
           </div>
           <div class="row">
@@ -181,10 +265,34 @@
               </div>
             </div>
             <div class="col q-pl-sm">
-              <q-field class="q-mt-md" :error="inst.dataAccess.$error" error-message="The level of data access is required">
-                <q-radio v-model.trim="inst.dataAccess.$model" val="no access" color="secondary" label="No Access" @input="update()"/>
-                <q-radio v-model.trim="inst.dataAccess.$model" val="anonymised" color="secondary" label="Anonymised" @input="update()" style="margin-left: 10px"/>
-                <q-radio v-model.trim="inst.dataAccess.$model" val="full" color="secondary" label="Full" @input="update()" style="margin-left: 10px"/>
+              <q-field
+                class="q-mt-md"
+                :error="inst.dataAccess.$error"
+                error-message="The level of data access is required"
+              >
+                <q-radio
+                  v-model.trim="inst.dataAccess.$model"
+                  val="no access"
+                  color="secondary"
+                  label="No Access"
+                  @input="update()"
+                />
+                <q-radio
+                  v-model.trim="inst.dataAccess.$model"
+                  val="anonymised"
+                  color="secondary"
+                  label="Anonymised"
+                  @input="update()"
+                  style="margin-left: 10px"
+                />
+                <q-radio
+                  v-model.trim="inst.dataAccess.$model"
+                  val="full"
+                  color="secondary"
+                  label="Full"
+                  @input="update()"
+                  style="margin-left: 10px"
+                />
               </q-field>
             </div>
           </div>
@@ -198,24 +306,43 @@
               </div>
             </div>
             <div class="col q-pl-sm">
-              <q-input-multilang type="textarea" v-model.trim="inst.reasonForDataAccess.$model" @blur="inst.reasonForDataAccess.$touch"
-              :readonly="inst.dataAccess.$model === 'no'"
-              @input="update()" :languages="value.languages" required/>
+              <q-input-multilang
+                type="textarea"
+                v-model.trim="inst.reasonForDataAccess.$model"
+                @blur="inst.reasonForDataAccess.$touch"
+                :readonly="inst.dataAccess.$model === 'no'"
+                @input="update()"
+                :languages="value.languages"
+                required
+              />
             </div>
           </div>
           <div class="row justify-center">
             <div class="col">
-              <q-btn v-show="index !=0" label="Remove Institution" color="negative" icon="remove" @click="removeRowInstitution(index)" />
+              <q-btn
+                v-show="index !=0"
+                label="Remove Institution"
+                color="negative"
+                icon="remove"
+                @click="removeRowInstitution(index)"
+              />
             </div>
             <div class="col">
-              <q-btn class="float-right" v-show="index == value.institutions.length-1" label="Add Institution" color="primary" icon="add" @click="addRowInstitution(index)" />
+              <q-btn
+                class="float-right"
+                v-show="index == value.institutions.length-1"
+                label="Add Institution"
+                color="primary"
+                icon="add"
+                @click="addRowInstitution(index)"
+              />
             </div>
           </div>
-          <q-separator v-show="index != value.institutions.length-1"/>
+          <q-separator v-show="index != value.institutions.length-1" />
         </div>
       </q-card-section>
     </q-card>
-<!-- Gen Info Tab: Date Card -->
+    <!-- Gen Info Tab: Date Card -->
     <q-card class="form-card">
       <q-card-section>
         <div class="text-h5">Dates</div>
@@ -232,9 +359,15 @@
             </div>
           </div>
           <div class="col q-pl-sm">
-            <q-input type="date" format="D-MMM-YYYY"
-            v-model.trim="v.startDate.$model" @blur="v.startDate.$touch" @input="update()"
-            :error="v.startDate.$error" error-message="Field is required."/>
+            <q-input
+              type="date"
+              format="D-MMM-YYYY"
+              v-model.trim="v.startDate.$model"
+              @blur="v.startDate.$touch"
+              @input="update()"
+              :error="v.startDate.$error"
+              error-message="Field is required."
+            />
           </div>
         </div>
         <div class="row">
@@ -247,9 +380,15 @@
             </div>
           </div>
           <div class="col q-pl-sm">
-            <q-input type="date" format="D-MMM-YYYY"
-            v-model.trim="v.endDate.$model" @blur="v.endDate.$touch" @input="update()"
-            :error="v.endDate.$error" error-message="Field is required."/>
+            <q-input
+              type="date"
+              format="D-MMM-YYYY"
+              v-model.trim="v.endDate.$model"
+              @blur="v.endDate.$touch"
+              @input="update()"
+              :error="v.endDate.$error"
+              error-message="Field is required."
+            />
           </div>
         </div>
       </q-card-section>
@@ -286,7 +425,11 @@ export default {
       this.value.institutions.push({
         name: '',
         contact: '',
-        dataAccess: ''
+        dataAccess: '',
+        reasonForDataAccess: {
+          en: '',
+          sv: ''
+        }
       })
     },
     removeRowInstitution (index) {
